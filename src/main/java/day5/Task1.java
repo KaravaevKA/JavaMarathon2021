93e9d4b70b293a0b772edd199e1203c850929c7a
Kirill Karavaev <coffee.2016@yandex.ru>
2023-08-12 10:16:37 +0300

    Complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,12 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        car.setModel("Ferrari");
+        car.setColor("Red");
+        car.setYearOfManufacture(2023);
+        System.out.println("Автомобиль: " + car.getModel());
+        System.out.println("Цвет: " + car.getColor());
+        System.out.println("Год выпуска: " + car.getYearOfManufacture());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,9 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Motorbike motorbike = new Motorbike("Honda", "Black", 2023);
+        System.out.println("Model: " + motorbike.getModel());
+        System.out.println("Color: " + motorbike.getColor());
+        System.out.println("Year of manufacture: " + motorbike.getYearOfManufacture());
     }
 }
