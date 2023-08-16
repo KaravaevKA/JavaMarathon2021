bc53e3c455b91585eb6ced3040c3d2a28ec1c434
Kirill Karavaev <coffee.2016@yandex.ru>
2023-08-16 18:55:29 +0300

    Complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,19 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        String str = "";
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            str = str + i + " ";
+        }
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы в мс: " + (stopTime - startTime));
+        StringBuilder stringBuilder = new StringBuilder();
+        long startTime1 = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            stringBuilder.append(i);
+        }
+        long stopTime1 = System.currentTimeMillis();
+        System.out.println("Длительность работы в мс: " + (stopTime1 - startTime1));
     }
 }
