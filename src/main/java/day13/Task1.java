086ee83c8d1579a1fb979bc867c560c5d27a8733
Kirill Karavaev <coffee.2016@yandex.ru>
2023-08-20 18:05:27 +0300

    Complete day13 tasks

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -2,6 +2,22 @@ package day13;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        MessageDatabase messageDatabase = new MessageDatabase();
+        User user = new User("Кирилл");
+        User user1 = new User("Иван");
+        User user2 = new User("Dmitrii");
+        user.sendMessage(user1,"Привет");
+        user.sendMessage(user1,"Как дела?");
+        user1.sendMessage(user,"Привет");
+        user1.sendMessage(user,"Да нормально. Сам как?");
+        user1.sendMessage(user,"Как жизнь?");
+        user2.sendMessage(user,"Привет");
+        user2.sendMessage(user,"Как сам?");
+        user2.sendMessage(user,"Как жизнь?");
+        user.sendMessage(user2,"Привет");
+        user.sendMessage(user2,"Отлично");
+        user.sendMessage(user2,"Все хорошо, а у тебя?");
+        user2.sendMessage(user,"Все путем");
+        MessageDatabase.showDialog(user,user2);
     }
 }
---
+++ src/main/java/day13/User.java
@@ -0,0 +1,61 @@
+package day13;
+
+import java.util.List;
+
+public class User {
+    private String userName;
+    private List<User> subscriptions;
+
+    private List<User> friendList;
+
+    public User(String userName) {
+        this.userName = userName;
+        this.subscriptions = subscriptions;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public List<User> getSubscriptions() {
+        return subscriptions;
+    }
+
+    public  void friendship( User user){
+            this.friendList.add(user);
+            user.friendList.add(this);
+    }
+
+    public void subscribe(User user){
+        subscriptions.add(user);
+        if (this.isSubscribed(user) && user.isSubscribed(this)){
+            friendship(user);
+        }
+
+    }
+
+    public boolean isSubscribed(User user){
+        if(this.subscriptions.contains(user)){
+            return true;
+        }else
+            return false;
+    }
+
+    public boolean isFriend(User user){
+        if (friendList.contains(user)){
+            return true;
+        }else
+            return false;
+    }
+
+    public void sendMessage(User user, String text){
+        MessageDatabase.addNewMessage(this,user,text);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                '}';
+    }
+}
