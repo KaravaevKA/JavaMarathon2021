385a16ffec2023898fdfffaa1ab3d9d4161cad94
Kirill Karavaev <coffee.2016@yandex.ru>
2023-08-30 23:15:46 +0300

    Complete day17 tasks

--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,47 @@
 package day16;
 
-public class Task2 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Task2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file1 = new File("file1.txt");
+        printResult(file1);
+    }
+
+    public static void printResult(File file) throws FileNotFoundException {
+        Random random = new Random();
+        PrintWriter printWriter = new PrintWriter(file);
+        for (int i = 0; i < 1000; i++) {
+            printWriter.println(random.nextInt(100));
+        }
+        printWriter.close();
+
+        Scanner scanner = new Scanner(file);
+        File file2 = new File("file2.txt");
+        PrintWriter printWriter1 = new PrintWriter(file2);
+        int counter = 0;
+        int sum = 0;
+        while (scanner.hasNextLine()){
+            sum += Integer.parseInt(scanner.nextLine());
+            counter++;
+            if(counter == 20){
+                printWriter1.println(sum/20.0);
+                counter = 0;
+                sum = 0;
+            }
+        }
+        printWriter1.close();
+        Scanner scanner1 = new Scanner(file2);
+        double result =0;
+        while (scanner1.hasNextLine()){
+            result += Double.parseDouble(scanner1.nextLine());
+        }
+        System.out.println((int) result);
     }
 }
--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,26 @@
 package day17;
 
+import java.util.ArrayList;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        ChessPiece pawnWhite1 = ChessPiece.PAWN_WHITE;
+        ChessPiece pawnWhite2 = ChessPiece.PAWN_WHITE;
+        ChessPiece pawnWhite3 = ChessPiece.PAWN_WHITE;
+        ChessPiece pawnWhite4 = ChessPiece.PAWN_WHITE;
+        ChessPiece rookBlack1 = ChessPiece.ROOK_BLACK;
+        ChessPiece rookBlack2 = ChessPiece.ROOK_BLACK;
+        ChessPiece rookBlack3 = ChessPiece.ROOK_BLACK;
+        ChessPiece rookBlack4 = ChessPiece.ROOK_BLACK;
+        ArrayList<ChessPiece> pieces = new ArrayList();
+        pieces.add(pawnWhite1);
+        pieces.add(pawnWhite2);
+        pieces.add(pawnWhite3);
+        pieces.add(pawnWhite4);
+        pieces.add(rookBlack1);
+        pieces.add(rookBlack2);
+        pieces.add(rookBlack3);
+        pieces.add(rookBlack4);
+        System.out.println(pieces);
     }
 }
