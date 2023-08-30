176e9bccf86d4b1beca404111b220baaa13922c8
Kirill Karavaev <coffee.2016@yandex.ru>
2023-08-30 23:14:35 +0300

    Complete day16 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -13,21 +13,19 @@ public class Task1 {
         File file = new File("src/main/resources/shoes.csv");
 
         try (PrintWriter printWriter = new PrintWriter("src/main/resources/missing shoes.txt", StandardCharsets.UTF_8);
-             Scanner scanner = new Scanner(file)){
-            File fileResult = new File("src/main/resources/data.txt");
-
-            while (scanner.hasNextLine()){
+             Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] shoe = line.split(";");
                 int count = Integer.parseInt(shoe[2]);
-                if (count == 0){
+                if (count == 0) {
                     printWriter.println(line);
                 }
             }
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("Некорректный формат входных данных");
         }
     }
 }
--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,30 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("numbers.txt");
+        printResult(file);
+    }
 
+    public static void printResult(File file){
+        try (Scanner scanner = new Scanner(file)){
+            float sum = 0;
+            float sumAvg = 0;
+            while (scanner.hasNextLine()){
+                String[] numbers = scanner.nextLine().split(" ");
+                for (int i = 0; i < numbers.length; i++) {
+                    sum += Integer.parseInt(numbers[i]);
+                }
+                sumAvg = sum / numbers.length;
+            }
+            System.out.println(sumAvg + " --> " + String.format("%.3f",sumAvg));
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
 
