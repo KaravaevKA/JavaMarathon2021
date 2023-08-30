ba7741b0c997df631b826389fb6ee4c11ed75a3b
Kirill Karavaev <coffee.2016@yandex.ru>
2023-08-30 23:16:53 +0300

    Complete day18 tasks

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -2,6 +2,14 @@ package day18;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+        System.out.println(recursionSum(numbers,numbers.length-1));
+    }
+    public static int recursionSum(int[] array, int num){
+        int sum = 0;
+        if (num == 0){
+            return array[num] + sum;
+        }
+        return array[num] + recursionSum(array,num-1);
     }
 }
--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -2,6 +2,20 @@ package day18;
 
 public class Task2 {
     public static void main(String[] args) {
+        int number = 717771237;
+        System.out.println(count7(number));
 
+    }
+
+    public static int count7(int num) {
+        int count = 0;
+        if (num / 10 == 0) {
+            if (num % 10 == 7)
+                count++;
+            return count;
+        }
+        if (num % 10 == 7)
+            count++;
+        return count + count7(num / 10);
     }
 }
